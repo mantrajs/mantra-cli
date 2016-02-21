f7eb17a019118d5140d329a82d73e0cbb3b6277a
Sung Won Cho <mikeswcho@gmail.com>
2016-02-21 16:24:14 +1100

    Generate method and publication

--- templates/server/publications/index.js
+++ templates/server/publications/index.js
@@ -1,3 +1,2 @@
-export default function() {
-
+export default function () {
 }
--- test/command_test.js
+++ test/command_test.js
@@ -118,4 +118,30 @@ describe("generate", function() {
       expect(indexContent).to.equal("import posts from \'./posts\';\n\nexport {\n  posts\n};\n");
     });
   });
+
+  describe("method", function() {
+    it("generates a method", function() {
+      commands.generate('method', 'posts');
+      expect(checkFileOrDirExists('./server/methods/posts.js')).to.equal(true);
+    });
+
+    it("updates server/methods/index.js", function() {
+      commands.generate('method', 'posts');
+      var indexContent = fs.readFileSync('./server/methods/index.js', {encoding: 'utf-8'});
+      expect(indexContent).to.equal("import posts from \'./posts\';\n\nexport default function () {\n  posts();\n}\n");
+    });
+  });
+
+  describe("publication", function() {
+    it("generates a publication", function() {
+      commands.generate('publication', 'posts');
+      expect(checkFileOrDirExists('./server/publications/posts.js')).to.equal(true);
+    });
+
+    it("updates server/publications/index.js", function() {
+      commands.generate('publication', 'posts');
+      var indexContent = fs.readFileSync('./server/publications/index.js', {encoding: 'utf-8'});
+      expect(indexContent).to.equal("import posts from \'./posts\';\n\nexport default function () {\n  posts();\n}\n");
+    });
+  });
 });
