a6204db5c083b7ba7b397ee32623a5de30592833
Sung Won Cho <mikeswcho@gmail.com>
2017-07-21 09:14:47 +1000

    Remove version from welcome page. Fix #119.

--- templates/client/modules/core/components/home.jsx
+++ templates/client/modules/core/components/home.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React from "react";
 
-const Home = () => (
+const Home = () =>
   <div>
     <h1>Mantra</h1>
-    <p>
-      Welcome to Mantra 0.4.2.
-    </p>
+    <p>Welcome to Mantra.</p>
     <ul>
       <li>
-        Read <a target="_blank" href="https://kadirahq.github.io/mantra/">spec</a>
+        Read{" "}
+        <a target="_blank" href="https://kadirahq.github.io/mantra/">
+          spec
+        </a>
       </li>
       <li>
-        Learn <a target="_blank" href="https://github.com/sungwoncho/mantra-cli#commands">CLI</a>
+        Learn{" "}
+        <a
+          target="_blank"
+          href="https://github.com/sungwoncho/mantra-cli#commands"
+        >
+          CLI
+        </a>
       </li>
     </ul>
-  </div>
-);
+  </div>;
 
 export default Home;
